4844fd1a507ce84fb5e7ef0a7eae546e1122b18d
Teodoro Garcia <tedtke514@gmail.com>
2021-01-16 19:59:58 -0600

    commit tree 85b98cf9d960b46fa595b2cd053b9bd6a168cada

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgURL = function fetchImg() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(json => imgs(json))
+}
+
+function imgs(pics){
+    dogHouse = document.getElementById('#dog-image-container')
+    pics.forEach(dog => {
+        dogHouse.appendChild(dog)
+    })
+}
